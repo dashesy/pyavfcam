c6dc52d5a0a91c7cf1d2048f81f945a4133d5b62
Ehsan Azarnasab <dashesy@gmail.com>
2015-09-13 15:49:55 -0700

    still image capture example

---
+++ capture_picture.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+Created on Sept 13, 2015
+@author: dashesy
+Purpose: Access AVFoundation as a Cython class
+"""
+
+import pyavfcam
+
+# Open the default video source
+cam = pyavfcam.AVFCam(sinks='image')
+cam.snap_picture('test.jpg')
+
+print "Saved test.jpg (Size: " + str(cam.shape[0]) + " x " + str(cam.shape[1]) + ")"
