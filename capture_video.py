1d440ce6df53a51fe6d83bc27ec0de5d5c3f77fa
Ehsan Azarnasab <dashesy@gmail.com>
2015-09-14 15:28:52 -0700

    example docstring

--- capture_video.py
+++ capture_video.py
@@ -3,12 +3,19 @@
 Created on Sept 7, 2015
 @author: dashesy
 Purpose: Access AVFoundation as a Cython class
+
+format:
+    python capture_video.py <video_name> [-duration <seconds>]
+options:
+    <video_name>: name of the video to record
+    -duration <seconds>: how many seconds to record
 """
 
 import sys
 import pyavfcam
 
 duration = 10
+video_name = None
 
 parg = None
 for arg in sys.argv:
@@ -19,10 +26,17 @@ for arg in sys.argv:
         continue
     if arg == '-duration':
         parg = arg
-    
+    elif video_name is None:
+        video_name = arg
+    else:
+        raise ValueError('Unknow argument %s' % arg)
+
+if video_name is None:
+    raise ValueError('capture_video.py <video_name> [-duration <seconds>]')
+
 # Open the default video source
 video = pyavfcam.AVFCam()
 if duration:
-    video.record('test.mov', duration=duration)
+    video.record(video_name, duration=duration)
 
-    print "Saved test.avi (Size: " + str(video.shape[0]) + " x " + str(video.shape[1]) + ")"
+    print "Saved " + video_name + " (Size: " + str(video.shape[0]) + " x " + str(video.shape[1]) + ")"
