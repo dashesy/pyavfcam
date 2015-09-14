b6f74a8da1faa9bc0f60cdee55762da323301742
Ehsan Azarnasab <dashesy@gmail.com>
2015-09-14 12:21:04 -0700

    initial image snap

--- src/modules/camera_frame.h
+++ src/modules/camera_frame.h
@@ -7,6 +7,9 @@
 // Keep the resemblence of a pure C++ header as much as possible
 //
 
+
+struct opaqueCFMutableDictionar;
+typedef struct opaqueCFMutableDictionar * CFMutableDictionaryRef;
 struct opaqueCMSampleBuffer;
 typedef struct opaqueCMSampleBuffer *CMSampleBufferRef;
 
@@ -14,6 +17,15 @@ typedef struct opaqueCMSampleBuffer *CMSampleBufferRef;
 class CameraFrame
 {
 public:
+    size_t m_bytesPerRow;
+    size_t m_width;
+    size_t m_height;
+    std::unique_ptr<char[]> m_img;
+
+private:
+    CFMutableDictionaryRef m_exif;
+public:
     CameraFrame(CMSampleBufferRef sampleBuffer);
+    virtual ~CameraFrame();
     void save(std::string path, std::string uti_type, float quality=1.0);
 };
--- src/modules/camera_frame.m
+++ src/modules/camera_frame.m
@@ -13,62 +13,110 @@
 
 // Input is a frame reference, make a solid object from it
 CameraFrame::CameraFrame(CMSampleBufferRef sampleBuffer)
+    : m_bytes_per_row(0), m_width(0), m_height(0), m_pImageData(NULL),
+      m_exif(NULL)
 {
     // Get a bitmap representation of the frame using CoreImage and Cocoa calls
 
     // Pass an actual reference to a custom Frame class up
 
-//        // Take this to re-attach if need to save
-//        CFDictionaryRef exifAttachments = CMGetAttachment(imageSampleBuffer, kCGImagePropertyExifDictionary, NULL);
-//        if (exifAttachments) {
-//            NSLog(@"attachements: %@", exifAttachments);
-//        } else {
-//            NSLog(@"no attachments");
-//        }
+    // Take exif to re-attach if need to save as image
+    CFDictionaryRef exifAttachments = CMGetAttachment(imageSampleBuffer, kCGImagePropertyExifDictionary, NULL);
+    if (exifAttachments) {
+        NSLog(@"attachements: %@", exifAttachments);
+        // Replace the previous copy (if any)
+        if (m_exif)
+            CFRelease(m_exif);
+        m_exif = CFDictionaryCreateMutableCopy(nil, 0, exifAttachments);
+    }
 
-//    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(source);
-//    CVPixelBufferLockBaseAddress(imageBuffer,0);
-//
-//    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
-//    size_t width = CVPixelBufferGetWidth(imageBuffer);
-//    size_t height = CVPixelBufferGetHeight(imageBuffer);
-//    void *src_buff = CVPixelBufferGetBaseAddress(imageBuffer);
-//
-//    NSData *data = [NSData dataWithBytes:src_buff length:bytesPerRow * height];
-//
-//    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
-//        UIImage * img = [[UIImage alloc] initWithData:d];
+    // Take the bitmap
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer,0);
+
+    m_bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    m_width = CVPixelBufferGetWidth(imageBuffer);
+    m_height = CVPixelBufferGetHeight(imageBuffer);
+    const char * src_buff = (const char *)CVPixelBufferGetBaseAddress(imageBuffer);
+    char * dst_buf = new char[m_bytesPerRow * m_height];
+    std::memcpy(dst_buf, src_buff, m_bytesPerRow * m_height);
+    m_img = std::unique_ptr<char[]>(dst_buf);
+
+    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
 }
 
+// Destructor
+CameraFrame::~CameraFrame()
+{
+    if (m_exif) {
+        CFRelease(m_exif);
+        m_exif = NULL;
+    }
+}
+
+// Save the frame to an image file
 void CameraFrame::save(std::string path, std::string uti_str, float quality)
 {
         NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
+        bool found_uti = true;
         CFStringRef uti_type = (CFStringRef)@"public.png";
         if (uti_str.length() == 0) {
+            found_uti = false;
             std::string ext = str_tolower(file_extension(path));
             if (ext == ".jpeg" || ext == ".jpg")
                 uti_type = (CFStringRef)@"public.jpeg";
-            // TODO: Can infere more uti mime
+                found_uti = true;
+            else if (ext == ".png") {
+                found_uti = true
         }
         // Get the string and expand it to a file URL
         NSString* path_str = [[NSString stringWithUTF8String:path.c_str()] stringByExpandingTildeInPath];
-        NSURL *url = [NSURL fileURLWithPath:path_str];
 
+        CFStringRef uti = NULL;
+        if (!found_uti) {
+            // Infere the uti
+            uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)[path_str pathExtension], NULL);
+            // If not known assume png
+            if (uti)
+                uti_type = uti;
+        }
 
-//        jpeg = ... is ext jpg
-//        CGImageRef imageRef = ...
-//        CFMutableDictionaryRef mSaveMetaAndOpts = NULL;
-//        CFStringRef uti_type = (CFStringRef)@"public.png";
-//        if (jpeg) {
-//            mSaveMetaAndOpts = CFDictionaryCreateMutable(nil, 0, &kCFTypeDictionaryKeyCallBacks,  &kCFTypeDictionaryValueCallBacks);
-//            CFDictionarySetValue(mSaveMetaAndOpts, kCGImageDestinationLossyCompressionQuality,
-//                                 [NSNumber numberWithFloat:quality]);
-//            uti_type = (CFStringRef)@"public.jpeg"
-//        }
-//
-//        CGImageDestinationRef dr = CGImageDestinationCreateWithURL((CFURLRef)outURL, uti_type, 1, NULL);
-//        CGImageDestinationAddImage(dr, imageRef, mSaveMetaAndOpts);
-//        CGImageDestinationFinalize(dr);
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+
+        CGContextRef newContext = CGBitmapContextCreate((CGImageRef) m_img.get(),
+                                                        m_width, m_height, 8, m_bytesPerRow, colorSpace,
+                                                        kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+        if (newContext) {
+            CGImageRef newImageRef = CGBitmapContextCreateImage(newContext); // It does a COW
+
+            if (newImageRef) {
+                CGColorSpaceRelease(colorSpace);
+
+                CFMutableDictionaryRef mSaveMetaAndOpts = NULL;
+                if (m_exif)
+                    mSaveMetaAndOpts = CFDictionaryCreateMutableCopy(nil, 0, m_exif);
+                else:
+                    mSaveMetaAndOpts = CFDictionaryCreateMutable(nil, 0, &kCFTypeDictionaryKeyCallBacks,  &kCFTypeDictionaryValueCallBacks);
+
+                // Set the image quality
+                CFDictionarySetValue(mSaveMetaAndOpts, kCGImageDestinationLossyCompressionQuality, [NSNumber numberWithFloat:quality]);
+
+                NSURL *url = [NSURL fileURLWithPath:path_str];
+                CGImageDestinationRef dr = CGImageDestinationCreateWithURL((__bridge CFURLRef)url, uti_type, 1, NULL);
+
+                CGImageDestinationAddImage(dr, newImageRef, mSaveMetaAndOpts);
+                CGImageDestinationFinalize(dr);
+
+                if (mSaveMetaAndOpts)
+                    CFRelease(mSaveMetaAndOpts);
+
+                CFRelease(newImageRef);
+            }
+            CGContextRelease(newContext);
+        }
+
+        if (uti)
+            CFRelease(uti);
         [pool drain];
 }
