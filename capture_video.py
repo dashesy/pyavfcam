68395a197345e67d0b2b819db28cfab7b0d6ef3e
Ehsan Azarnasab <dashesy@gmail.com>
2015-09-21 14:42:08 -0700

    option to record video in a QThread for test

--- capture_video.py
+++ capture_video.py
@@ -9,11 +9,13 @@ format:
 options:
     <video_name>: name of the video to record
     -duration <seconds>: how many seconds to record
+    -qthread: run recording in a QThread
 """
 
 import sys
 import pyavfcam
 
+threaded = False
 duration = 10
 video_name = None
 
@@ -28,6 +30,8 @@ for arg_idx, arg in enumerate(sys.argv):
         continue
     if arg == '-duration':
         parg = arg
+    elif arg == '-qthread':
+        threaded = True
     elif video_name is None:
         video_name = arg
     else:
@@ -36,9 +40,53 @@ for arg_idx, arg in enumerate(sys.argv):
 if video_name is None:
     raise ValueError('capture_video.py <video_name> [-duration <seconds>]')
 
-# Open the default video source
-cam = pyavfcam.AVFCam()
-if duration:
-    cam.record(video_name, duration=duration)
 
-    print "Saved " + video_name + " (Size: " + str(cam.shape[0]) + " x " + str(cam.shape[1]) + ")"
+BaseClass = object
+if threaded:
+    try:
+        # noinspection PyPackageRequirements
+        from PySide import QtCore
+    except ImportError:
+        # noinspection PyPackageRequirements
+        from PyQt4 import QtCore
+        # noinspection PyUnresolvedReferences
+        QtCore.Signal = QtCore.pyqtSignal
+        # noinspection PyUnresolvedReferences
+        QtCore.Slot = QtCore.pyqtSlot
+    BaseClass = QtCore.QObject
+
+
+class Worker(BaseClass):
+
+    # Task done with list of bpm results
+    taskDone = QtCore.Signal(list)
+
+    def __init__(self):
+        """run an app job in a thread
+        """
+        super(Worker, self).__init__()
+
+        self.t = None
+        if threaded:
+            self.t = QtCore.QThread(objectName='record_thread')
+            self.moveToThread(self.t)
+            self.t.start()
+            # noinspection PyUnresolvedReferences
+            self.t.started.connect(self.task)
+
+    def task(self):
+        if self.t:
+            thread_name = QtCore.QThread.currentThread().objectName()
+            print '[%s] recording' % thread_name
+
+        # Open the default video source and record
+        cam = pyavfcam.AVFCam()
+        if not duration:
+            return
+        cam.record(video_name, duration=duration)
+        print "Saved " + video_name + " (Size: " + str(cam.shape[0]) + " x " + str(cam.shape[1]) + ")"
+
+
+w = Worker()
+if not threaded:
+    w.task()
