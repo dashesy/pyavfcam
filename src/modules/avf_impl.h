e68db92b7dcaba20076a5f9237567d45f82dae8f
Ehsan Azarnasab <dashesy@gmail.com>
2015-09-22 11:37:39 -0700

    set public methods and instance vars in pimpl

--- src/modules/avf_impl.h
+++ src/modules/avf_impl.h
@@ -10,18 +10,25 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
+class CppAVFCam;
+
 // A basic shim that just passes things to C++ instance
 @interface AVCaptureDelegate : NSObject <AVCaptureFileOutputRecordingDelegate,
                                          AVCaptureVideoDataOutputSampleBufferDelegate>
 {
     CppAVFCam * instance; // What I am delegated for
     NSTimer *timer; // Keep-alive timer
+
+@public
     AVCaptureSession * m_pSession;
     AVCaptureDevice * m_pDevice;              // Camera device
     AVCaptureDeviceInput * m_pVideoInput;
     AVCaptureMovieFileOutput * m_pVideoFileOutput;
     AVCaptureStillImageOutput * m_pStillImageOutput;
 }
+
+- (void)setInstance:(CppAVFCam *)pInstance;
+- (id)initWithInstance:(CppAVFCam *)pInstance;
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput
   didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
